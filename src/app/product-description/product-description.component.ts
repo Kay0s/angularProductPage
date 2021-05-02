1fd34832b3635184676b8923d57dc4d1e171c01f
Kay0s <hamilton.kristina@gmail.com>
2021-05-01 19:08:33 -0500

    updated ngOnInit() function

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -8,13 +8,11 @@ import { ProductService } from '../product.service';
   styleUrls: ['./product-description.component.css']
 })
 export class ProductDescriptionComponent implements OnInit {
-  albumInfo: Album;
+  albumInfo;
   constructor(private _productService: ProductService) { }
 
   ngOnInit() {
-    this._productService
-      .getAlbum(1)
-      .subscribe((response) => (this.albumInfo = response));
+    this._productService.getAlbum(1).subscribe(response => this.albumInfo = response);
 
   }
 
